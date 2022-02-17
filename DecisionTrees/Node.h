d2382e046d4992e5863bdf32ae18616b17c1c36f
Fibonacci <polymorphic310@outlook.com>
2022-02-17 13:01:53 +0000

    Add project files.

---
+++ DecisionTrees/Node.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+#include <functional>
+
+class Node
+{
+public:
+	// Constructor - saves the custom function passed into the class when created
+	Node(std::function<int()> new_custom_func) : custom_func(new_custom_func) {}
+
+	// Basic node just calls its custom function when run() is called
+	virtual void run() {
+		custom_func();
+	}
+
+	// Adds a child node to a vector
+	void addChildNode(Node* new_child) { child_nodes.push_back(new_child); }
+protected:
+	std::vector<Node*> child_nodes;		// Vector of child nodes (branches or leaves)
+	std::function<int()> custom_func;	// Custom function that tells the node what to do
+};
+
+class DecisionNode : public Node
+{
+public:
+	// Constructor - saves the custom function passed into the class when created
+	DecisionNode(std::function<int()> new_custom_func) : Node(new_custom_func) {}
+
+	// Override the run() function
+	// custom_func() needs to be designed to return an index to which child branch to run
+	// Runs that child branch if it can access it
+	void run() override {
+		int index = custom_func();
+		if (index >= 0 && index < child_nodes.size())
+		{
+			Node* child = child_nodes[index];
+			child->run();
+		}
+	}
+};
---
+++ DecisionTrees/Source.cpp
@@ -0,0 +1,17 @@
+#include "Node.h"
+#include <iostream>
+#include <thread>
+using namespace std::chrono_literals;
+
+int main()
+{
+	srand(time(NULL));
+
+	// Create functions / lambdas for each unique node in the tree
+
+	// Create instances of deciusion nodes and leaf nodes as appropriate
+
+	// Add the leaf nodes as children to the connected decision nodes
+
+	// Run the root node of the tree
+}
